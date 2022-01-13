4b4c14a5dff5a8a84a097cda3b6ed6796a5b1fbc
mj-fouch <foucher.jeremy.91@gmail.com>
2022-01-13 11:21:04 +0100

    message

---
+++ src/fr/main/jeremy/Main.java
@@ -0,0 +1,18 @@
+package fr.main.jeremy;
+
+import fr.exo.Imprimante;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Imprimante imprimante = Imprimante.getInstance();
+		imprimante.ajoutPapier(3);
+		imprimante.imprimer();
+		imprimante.imprimer();
+		imprimante.imprimer();
+		imprimante.imprimer();
+		Imprimante imprimanteDeux = Imprimante.getInstance();
+		imprimanteDeux.imprimer();
+	}
+
+}
